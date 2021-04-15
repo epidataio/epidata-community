6f718622ba014bbb00a4494582e5a550aab8785f
Arushi <arushi.a052@gmail.com>
2021-04-14 17:41:40 -0700

    pandas import

--- ipython/epidata/EpidataLiteContext.py
+++ ipython/epidata/EpidataLiteContext.py
@@ -12,6 +12,7 @@ from threading import Thread
 #from streaming import EpidataStreamingContext
 from _private.transformation import Transformation
 from py4j.java_gateway import JavaGateway
+import pandas as pd
 
 '''
 from py4j.java_gateway import JavaGateway 
@@ -30,11 +31,7 @@ class EpidataLiteContext:
         #java_import(self._sc._jvm, "com.epidata.spark.EpidataContext")
         #self._jec = self._sc._jvm.EpidataContext(self._sc._jsc)
         #other confs and connections
-<<<<<<< HEAD
         gg = gateway.launch_gateway(classpath="../../spark/target/scala-2.12/epidata-spark_2.12-1.0-SNAPSHOT.jar")
-=======
-        gg = gate.launch_gateway(classpath="spark/target/scala-2.12/epidata-spark_2.12-1.0-SNAPSHOT.jar")
->>>>>>> 95dec4e7596f265274d38cd4d73c0a437b553f64
         java_entry = gg.jvm.com.epidata.spark.EpidataLiteContext() 
 
         
