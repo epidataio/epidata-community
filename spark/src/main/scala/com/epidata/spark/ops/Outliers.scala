20eb6f692e91a028ba0310eb33f16ea6f8e1df9d
MarinaLinn6 <qlin3628@usc.edu>
2022-08-19 10:25:49 -0700

    outliers modified

--- spark/src/main/scala/com/epidata/spark/ops/Outliers.scala
+++ spark/src/main/scala/com/epidata/spark/ops/Outliers.scala
@@ -24,10 +24,10 @@ class Outliers(
     val method: String) extends Transformation {
 
   override def apply(measurements: ListBuffer[JLinkedHashMap[String, Object]]): ListBuffer[JLinkedHashMap[String, Object]] = {
-    var dataset = collection.immutable.Map[String, List[Double]]()
+    var dataset = collection.immutable.Map[String, util.ArrayList[java.lang.Double]]()
     for (mfield <- fields) {
       if (!dataset.contains(mfield)) {
-        val empty: List[Object] = List()
+        var empty = new util.ArrayList[java.lang.Double]()
         dataset + (mfield -> empty)
       }
       for (index <- measurements.indices) {
@@ -39,7 +39,6 @@ class Outliers(
       }
 
     }
-    var list = new java.util.ArrayList[Int]()
     var outliers = new ListBuffer[Int]()
     for (index <- measurements.indices) {
 
@@ -47,12 +46,12 @@ class Outliers(
         val fieldName = measurements(index).get("meas_name").asInstanceOf[String]
         var q_low: Double = 0.0
         var q_high: Double = 0.0
-        val mdataset: List[Double] = dataset.get(fieldName) match {
+        val mdataset: util.ArrayList[java.lang.Double] = dataset.get(fieldName) match {
           //case None => None //Or handle the lack of a value another way: throw an error, etc.
-          case Some(s: List[Double]) => s //return the string to set your value
+          case Some(s: util.ArrayList[java.lang.Double]) => s //return the string to set your value
         }
-        //        q_low = percentiles().index(mpercentage).compute(list)
-        //        q_high = percentiles().index(1 - mpercentage).compute(mdataset.asJava)
+        q_low = percentiles().index(mpercentage).compute(mdataset)
+        q_high = percentiles().index(1 - mpercentage).compute(mdataset)
         val currData = measurements(index).get("meas_value").asInstanceOf[Double]
         if (currData < q_low || currData > q_high) {
           outliers += index
