a5192e6835f7a7960ee44b2ed123a0c2cb3efaf6
srinibadri <srini.badri01@gmail.com>
2022-09-30 23:34:37 -0700

    notebook launch script updated

--- ipython/start_notebook_lite.sh
+++ ipython/start_notebook_lite.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2015-2022 EpiData, Inc.
 #
 
-export EPIDATA_HOME="C:/Users/Raj Sunku/OneDrive - UC San Diego/Epidata/epidata-community"
+export EPIDATA_HOME="../"
 export EPIDATA_PYTHON_HOME=$EPIDATA_HOME/python
 export EPIDATA_IPYTHON_HOME=$EPIDATA_HOME/ipython
 export PYTHONPATH="$PYTHONPATH;$EPIDATA_IPYTHON_HOME;$EPIDATA_IPYTHON_HOME/epidata;$EPIDATA_IPYTHON_HOME/epidata/_private;$EPIDATA_IPYTHON_HOME/epidata/py4j-0.10.9.2/src/py4j;$EPIDATA_PYTHON_HOME/epidata_common;$EPIDATA_PYTHON_HOME"
