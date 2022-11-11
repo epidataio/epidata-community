c5caa84110b507a78f6050459931f51896aeac6a
srinibadri <srini.badri01@gmail.com>
2022-11-10 19:55:27 -0800

    updated basepath

--- ipython/start_notebook_lite.sh
+++ ipython/start_notebook_lite.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2015-2022 EpiData, Inc.
 #
 
-export EPIDATA_HOME="../"
+export EPIDATA_HOME="/Users/srinibadri/Documents/Repos/epidata/epidata-community-interns"
 export EPIDATA_PYTHON_HOME=$EPIDATA_HOME/python
 export EPIDATA_IPYTHON_HOME=$EPIDATA_HOME/ipython
 #export PYTHONPATH="$PYTHONPATH;$EPIDATA_IPYTHON_HOME;$EPIDATA_IPYTHON_HOME/epidata;$EPIDATA_IPYTHON_HOME/epidata/_private;$EPIDATA_IPYTHON_HOME/epidata/py4j-0.10.9.2/src/py4j;$EPIDATA_PYTHON_HOME/epidata_common;$EPIDATA_PYTHON_HOME"
@@ -13,5 +13,6 @@ export EPIDATA_LITE_JAR="$EPIDATA_HOME/spark/target/scala-2.12/epidata-spark-ass
 
 echo $PYTHONPATH
 
-# jupyter notebook --config config.py
+ps -ef | grep epidata-spark-assembly | grep -v grep | awk '{print $2}' | xargs kill
+
 (java -jar "$EPIDATA_LITE_JAR") & jupyter notebook --config config.py
